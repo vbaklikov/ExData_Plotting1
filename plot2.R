3551bccc4c842c08be0fbeb2436417621a32ada8
Vitaliy Baklikov <vbaklikov@gmail.com>
2014-05-07 17:53:54 +0100

    added plot3 construction

--- plot2.R
+++ plot2.R
@@ -3,8 +3,9 @@ library(sqldf)
 sql <- "SELECT * from file WHERE Date='1/2/2007' or Date='2/2/2007'"
 myData <- read.csv2.sql("household_power_consumption.txt", sql)
 
-myData$datetime <- as.POSIXct(paste(myData$Date, myData$Time),"%d/%m/%Y %H:%M:%S")
+myData$datetime <- as.POSIXct(paste(myData$Date, myData$Time),format="%d/%m/%Y %H:%M:%S")
 
+par(mar = c(5,5,2,2))
 plot(x=myData$datetime, y=myData$Global_active_power, pch=".", type="o", xlab="", ylab="Global Active Power(kilowatts)")
 
 dev.copy(png, file = "plot2.png")
---
+++ plot3.R
@@ -0,0 +1,18 @@
+library(sqldf)
+
+sql <- "SELECT * from file WHERE Date='1/2/2007' or Date='2/2/2007'"
+myData <- read.csv2.sql("household_power_consumption.txt", sql)
+
+myData$datetime <- as.POSIXct(paste(myData$Date, myData$Time),format="%d/%m/%Y %H:%M:%S")
+
+par(mar = c(5,5,2,2))
+plot(x=myData$datetime, y=myData$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering")
+
+points(x=myData$datetime,y=myData$Sub_metering_1,pch=".",type="o")
+points(x=myData$datetime,y=myData$Sub_metering_2,pch=".",type="o",col="red")
+points(x=myData$datetime,y=myData$Sub_metering_3,pch=".",type="o",col="blue")
+
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), pch="_",col=c("black","red","blue"))
+
+dev.copy(png, file = "plot3.png")
+dev.off()
