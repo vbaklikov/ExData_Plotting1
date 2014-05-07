1c12a757489aef8649edc7b4919fd1510445ccc6
Vitaliy Baklikov <vbaklikov@gmail.com>
2014-05-07 17:15:13 +0100

    built plot for part2

---
+++ plot2.R
@@ -0,0 +1,11 @@
+library(sqldf)
+
+sql <- "SELECT * from file WHERE Date='1/2/2007' or Date='2/2/2007'"
+myData <- read.csv2.sql("household_power_consumption.txt", sql)
+
+myData$datetime <- as.POSIXct(paste(myData$Date, myData$Time),"%d/%m/%Y %H:%M:%S")
+
+plot(x=myData$datetime, y=myData$Global_active_power, pch=".", type="o", xlab="", ylab="Global Active Power(kilowatts)")
+
+dev.copy(png, file = "plot2.png")
+dev.off()
